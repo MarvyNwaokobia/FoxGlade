5f3723f24ae8dda93eef61d4ec92ab746a9c7da8
marvy <marvynwaokobia@gmail.com>
2026-08-20 10:32:01 +0100

    Revert "Show immediate feedback when tapping Connect Wallet instead of a silent dim"
    This reverts commit 72d7decb2cc6a7a2196cbfd81e9fdff5a01215f1.

--- apps/web/src/components/ConnectGate.tsx
+++ apps/web/src/components/ConnectGate.tsx
@@ -199,7 +199,7 @@ export function ConnectGate({ checking }: { checking: boolean }) {
                   onClick={handleConnectWallet}
                   disabled={sending}
                 >
-                  {sending ? "CONNECTING…" : "CONNECT WALLET"}
+                  CONNECT WALLET
                 </button>
               </>
             )}
